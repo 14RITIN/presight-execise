1023dbfa3f129dcaeb16b345c21a97acde510aae
ritin kotian <ritin.kotian@presight.ai>
2026-09-13 18:36:50 +0400

    flicker issue fixed

--- client/src/features/users/components/UserGrid.tsx
+++ client/src/features/users/components/UserGrid.tsx
@@ -17,16 +17,18 @@ export function UserGrid({
   onLoadMore,
 }: UserGridProps) {
   return (
-    <VirtuosoGrid
-      useWindowScroll
-      data={users}
-      endReached={() => {
-        if (hasNextPage && !isFetchingNextPage) {
-          onLoadMore();
-        }
-      }}
-      listClassName="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
-      itemContent={(_, user) => <UserCard user={user} />}
-    />
+   <VirtuosoGrid
+  useWindowScroll
+  data={users}
+  listClassName="grid grid-cols-1 items-stretch gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+  itemClassName="h-full"
+  computeItemKey={(_, user) => user.id}
+  endReached={() => {
+    if (hasNextPage && !isFetchingNextPage) {
+      onLoadMore();
+    }
+  }}
+  itemContent={(_, user) => <UserCard user={user} />}
+/>
   );
 }

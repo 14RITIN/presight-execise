e20eca1f3a77159d0a185b78b8ff86b4db7249cf
ritin kotian <ritin.kotian@presight.ai>
2026-09-14 09:55:48 +0400

    data folder created on init

--- server/src/db/database.ts
+++ server/src/db/database.ts
@@ -1,7 +1,12 @@
-import Database from 'better-sqlite3';
+import fs from 'node:fs';
 import path from 'node:path';
+import Database from 'better-sqlite3';
 
 const databasePath = path.resolve(__dirname, '../../data/app.db');
+
+fs.mkdirSync(path.dirname(databasePath), {
+  recursive: true,
+});
 
 export const db = new Database(databasePath);
 
